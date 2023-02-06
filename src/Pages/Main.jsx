ebf5eac40ebe2994cc1800905bd2b46f66deb900
anikdev2016 <anikdev2016@gmail.com>
2023-02-06 09:16:09 +0600

    completed half of top nav

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import TopNav from "./SharedPages/TopNav";
 
 const Main = () => {
   return (
     <div>
+      {/* nav */}
+      <TopNav />
       <Outlet />
     </div>
   );
---
+++ src/Pages/SharedPages/TopNav.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
+import styles from "./TopNav.module.css";
+const TopNav = () => {
+  const [inputContainerClass, setInputContainerClass] = useState(false);
+  const searchBar = (
+    <>
+      <div
+        className={`border-2 border-gray-200 rounded-md w-96 h-10 flex justify-between px-4 items-center ${
+          inputContainerClass && styles.topNav__searchContainer
+        }`}
+      >
+        <input
+          onFocus={() => setInputContainerClass(true)}
+          type="text"
+          placeholder="Search..."
+          className={`w-10/12 h-full border-none outline-none focus-visible:border-none focus:border-none bg-transparent ${styles.topNav__searchBar}`}
+        />
+        <div className="cursor-pointer">
+          <BiSearchAlt2 size={25} />
+        </div>
+      </div>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li tabIndex={0}>
+              <a className="justify-between">
+                Parent
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        {/* search input */}
+        {searchBar}
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Get started</a>
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
